4228bcb55babe3de1e95c8bb27fd468c2f855eff
lapaduga <47709041+lapaduga@users.noreply.github.com>
2020-07-07 12:38:11 +0300

    Base version
    base

---
+++ js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	$('#send-data').on('click', function (e) {
+
+		if($('#input-1').val() == ''){
+			e.preventDefault();
+			$('#input-1').css('border', '1px solid red');
+		} else{
+			$('#input-1').css('border', '1px solid #ddd');
+		}
+
+		if($('#input-2').val() == ''){
+			e.preventDefault();
+			$('#input-2').css('border', '1px solid red');
+		}else{
+			$('#input-1').css('border', '1px solid #ddd');
+		}
+
+		if($('#input-3').val() == ''){
+			e.preventDefault();
+			$('#input-3').css('border', '1px solid red');
+		}else{
+			$('#input-1').css('border', '1px solid #ddd');
+		}
+	});
+});
